f76a445f73f7f9f00d3cf1f2f5230e510388345e
Elise Dang <elisehdang@gmail.com>
2018-06-22 16:57:17 -0700

    Fixed jumpy scrolling from nav bar

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
+// code inside will only run once the page DOM is ready for Javascript code to execute
 $(document).ready(function(){
    var offset = $(".navbar").offset().top;
+   console.log(offset);
+
+   // scroll event is sent to window when the user scrolls to a different place in the window
    $(window).scroll(function() {
+         // if the vertical scroll position (the number of pixels that are hidden from view
+         //  above the scrollable area) is >= offset (height of navbar)
          if ($(window).scrollTop() >= offset) {
              $('.navbar').addClass('navbar-fixed-top');
          }
-         // else {
-         //     $('.navbar').removeClass('navbar-fixed-top');
-         // }
     });
 
+   // "click.smoothscroll" is a namespace
    $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+       // prevents the default action of the event to not be triggered
+       //  e.g., clicked anchors won't take the browser to a new URL
        e.preventDefault();
 
-       // figuring out which element to scroll to
+       // setting target to the anchor part of the URL... $() is shorthand of jQuery()
        var target = this.hash,
            $target = $(target);
 
       var navbarHeightPixel = 35;
       var targetOffset = $target.offset().top - navbarHeightPixel;
+      // .animate(properties [, duration ] [, easing ] [, complete ] )
+      // properties: an object of CSS properities and values that the animation will move toward
+      // duration: how long the animation will run
+      // easing: which easing function to use for the transition
+      // complete: a function to call once the animation is complete, called once per matched element
        $('html, body').animate({
            'scrollTop': targetOffset
-       }, 1200, 'swing', function () {
-           window.location.hash = target;
-       });
+       }, 1200, 'swing');
    });
   });
